bce30f502d4e4d916688bc346bad334efb3a4948
vaufreyd <vaufreyd@c4bd1236-c4f7-0310-a2fe-a60683d9aa4a>
2006-09-11 11:27:41 +0000

    git-svn-id: svn+ssh://oberon/svn/OMiSCID-Dev@1253 c4bd1236-c4f7-0310-a2fe-a60683d9aa4a

---
+++ Examples/TestThread.cpp
@@ -0,0 +1,36 @@
+#include <System/Thread.h>
+
+using namespace Omiscid;
+
+class TimerProvider : public Thread
+{
+public:
+	TimerProvider()
+	{
+	};
+
+	virtual ~TimerProvider() {};
+
+	void Run()
+	{
+		unsigned int TimerNumber = 0;
+		
+		while( !StopPending() )
+		{
+			printf( "%u\n", ++TimerNumber );
+			Sleep(1000);
+		}
+	}
+
+};
+
+int main( int arcg, char* argv[] )
+{
+	TimerProvider TP;
+
+	TP.StartThread();
+
+	Thread::Sleep(10000);
+
+	TP.StopThread(0);
+}
