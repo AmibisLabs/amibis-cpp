292a467b1a4d89e5bbf4088cec13afb875646e25
vaufreyd <vaufreyd@c4bd1236-c4f7-0310-a2fe-a60683d9aa4a>
2006-09-14 11:32:58 +0000

    add Config.cpp
    git-svn-id: svn+ssh://oberon/svn/OMiSCID-Dev@1306 c4bd1236-c4f7-0310-a2fe-a60683d9aa4a

---
+++ ServiceControl/Config.cpp
@@ -0,0 +1,23 @@
+/** 
+ * @defgroup ServiceControl Layer 2 : Omiscid multiplateform Service Control layer. 
+ *
+ */
+
+/** 
+ * @file ServiceControl/Config.cpp
+ * @ingroup ServiceControl
+ * @brief defines global values for the ServiceControl layer
+ */
+
+#include <ServiceControl/Config.h>
+
+using namespace Omiscid;
+
+// Constant string values for service registration
+SimpleString Omiscid::LockString("lock");
+SimpleString Omiscid::NameString("name");
+SimpleString Omiscid::OwnerString("owner");
+SimpleString Omiscid::ClassString("class");
+SimpleString Omiscid::PeerIdString("peerId");
+
+
