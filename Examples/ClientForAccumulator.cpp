6f4f9ab2c589e04570dbbb4a657cf7ee30973a0a
vaufreyd <vaufreyd@c4bd1236-c4f7-0310-a2fe-a60683d9aa4a>
2006-09-15 08:58:31 +0000

    git-svn-id: svn+ssh://oberon/svn/OMiSCID-Dev@1337 c4bd1236-c4f7-0310-a2fe-a60683d9aa4a

---
+++ Examples/ClientForAccumulator.cpp
@@ -0,0 +1,185 @@
+/**
+ * @file Examples/Accumulator.cpp
+ * @ingroup Examples
+ * @brief Demonstartion of usage of communication between services.
+ *
+ * @author Dominique Vaufreydaz
+ */
+
+// Standard includes
+#include <System/Mutex.h>
+#include <ServiceControl/ConnectorListener.h>
+#include <ServiceControl/Factory.h>
+#include <ServiceControl/Service.h>
+#include <ServiceControl/ServiceProxy.h>
+
+#include <iostream>
+using namespace std;
+
+using namespace Omiscid;
+
+namespace Omiscid {
+
+/**
+ * @class ClientConnectorListener
+ * @ingroup Examples
+ * @brief this class is used to monitor incomming activity the Accumalator
+ */
+class ClientConnectorListener : public ConnectorListener
+{
+public:
+	/* @brief constructor */
+	ClientConnectorListener();
+	/* @brief destructor */
+	virtual ~ClientConnectorListener();
+
+	/* @brief callback function overide to receive data */
+	void MessageReceived(Service& TheService, const SimpleString LocalConnectorName, const Message& Msg);
+};
+
+/**
+ * @class VariableListener
+ * @ingroup Examples
+ * @brief this class is used to monitor remote variable changes
+ */
+class AccuChange : public RemoteVariableChangeListener
+{
+public:
+    /**
+     * Notification of a value change on a variable. Overide here because it is pur virtual.
+     *
+     * @param SP the ServiceProxy containing the variable
+     * @param VarName the name of the variable
+     * @param NewValue the new value of the variable
+     */
+    void VariableChanged(ServiceProxy& SP, const SimpleString VarName, const SimpleString NewValue )
+	{
+		cout << "Current Accu value: " << NewValue << endl;
+	};
+};
+
+} // namespace Omiscid
+
+	/* @brief constructor */
+ClientConnectorListener::ClientConnectorListener()
+{
+}
+
+	/* @brief destructor */
+ClientConnectorListener::~ClientConnectorListener()
+{
+}
+
+void ClientConnectorListener::MessageReceived(Service& TheService, const SimpleString LocalConnectorName, const Message& Msg)
+{
+	// Create a SimpleString with message and output it
+	// even if we can directly output it. Show usage of SimpleString
+
+	SimpleString Message = Msg.GetBuffer();
+
+	cerr << Message << endl;
+}
+
+
+
+/* @brief main program entry for the Accumulator. No need to give parameter */
+int main(int argc, char*argv[] )
+{
+	// Instanciate a Connector listener (must *not* be destroy before it has
+	// been removed for the connector or until the service is destroyed).
+	ClientConnectorListener MyCommandListener;
+
+	// Instanciate a Connector listener (must *not* be destroy before it has
+	// been removed for the connector or until the service is destroyed).
+	AccuChange MyAccuListener;
+	
+	// Ask to the service factory to create a Service. The service is not
+	// register yet. We do not provide the service class, the default value 'Service'
+	// will be used
+	Omiscid::Service * pAccuClient = ServiceFactory.Create( "Accumulator Client" );
+
+	// If something tricky occurred, exit
+	if ( pAccuClient == NULL )
+	{
+		fprintf( stderr, "Could not create the Accumulator Client service.\n" );
+
+		// Exit the thread
+		return -1;
+	}
+
+	// Add a Connector to send commands and receive error messages (AnInOutput) and set my callback
+	// object to receive notification of messages
+	pAccuClient->AddConnector( "SendCommands", "A way to send commands to the Accumulator", AnInOutput );
+	pAccuClient->AddConnectorListener( "SendCommands", &MyCommandListener );
+
+	// Trace
+	printf( "Accumulator Client created.\n" );
+
+	// A proxy to communicate with the Accumulator
+	ServiceProxy * OneAccumulator;
+
+	// Loop for searching an Accumulator
+	printf( "Search for an Accumulator service.\n" );
+	for( int NbTry = 1; NbTry <= 5; NbTry ++ )
+	{
+		// Search for services, wait 5 s (5000 ms) to get an answer
+		// Get in return a ServiceProxy List. This service is nammed Accumalator, gets a variable
+		// Accu and an InOutput connector "Commands"
+		OneAccumulator = pAccuClient->FindService( And(NameIs("Accumulator"),HasVariable("Accu"), HasConnector("Commands",AnInOutput) ), 5000 );
+
+		if ( OneAccumulator == NULL )
+		{
+			fprintf( stderr, "Search for an Accumulator service failed. Try again...\n" );
+			continue;
+		}
+
+		break;
+	}
+
+	if ( OneAccumulator == NULL )
+	{
+		fprintf( stderr, "Could not find an Accumulator service. quit !\n" );
+
+		// delete 
+		delete pAccuClient;
+		return -1;
+	}
+
+	// register the Client service and launch everything
+	// we must do it before communicating with the Accumalator
+	pAccuClient->Start();
+
+	// Connect to the Accumulator
+	if ( pAccuClient->ConnectTo( "SendCommands", OneAccumulator, "Commands" ) == true )
+	{
+		// Subscribe to variables changes for Accu. I will receive a first callback
+		// when done
+		OneAccumulator->AddRemoteVariableChangeListener( "Accu", &MyAccuListener );
+
+		// Enter command prompt, 1er time with help
+		cout << "Enter new command ('+4', '/5.5', ' *8.59', '-6', ' = 8.2'...): " << endl;
+		
+		// Do my work
+		SimpleString LocalCommand;
+		for(;;)
+		{
+			// Get the local command at keyboard
+			cin >> LocalCommand;
+
+			// Send to all connected Client... Here we aqre connected ton only one accumulator
+			// we can also use SentToOneClient...
+			pAccuClient->SendToAllClients( "SendCommands", (char*)LocalCommand.GetStr(), LocalCommand.GetLength(), false );
+		}
+	}
+	else
+	{
+		fprintf( stderr, "Could not connect to the Accumulator connector. quit !\n" );
+	}
+
+	// cleanup everything
+	delete pAccuClient;
+	delete OneAccumulator;
+
+	// exit
+	return 0;
+}
