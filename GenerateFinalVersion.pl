6085aaac9018987b620bd1ecf7f75d0ce99d2ac3
vaufreyd <vaufreyd@c4bd1236-c4f7-0310-a2fe-a60683d9aa4a>
2007-04-27 11:02:53 +0000

    GenerateFinalVersion.pl and CHANGES up to date
    git-svn-id: svn+ssh://oberon/svn/OMiSCID-Dev@2445 c4bd1236-c4f7-0310-a2fe-a60683d9aa4a

--- GenerateFinalVersion.pl
+++ GenerateFinalVersion.pl
@@ -26,31 +26,39 @@ sub WorkOnFile()
 	$FilesToAdd{$FileName} = 1;
 }
 
+# default values for parameters
 $IncrVersionType = 0;	# last
-if ( defined $ARGV[0] )
+$DoTest = 1;
+
+$PosArgv = 0;
+while( defined $ARGV[$PosArgv] )
 {
-	if ( $ARGV[0] eq '-middle' )
+	if ( $ARGV[$PosArgv] eq '-middle' )
 	{
 		$IncrVersionType = 1; # middle number
+		$PosArgv++;
+		next;
 	}
-	else
+	if ( $ARGV[$PosArgv] eq '-major' )
 	{
-		if ( $ARGV[0] eq '-major' )
-		{
-			$IncrVersionType = 2; # major number
-		}
-		else
-		{
-			if ( $ARGV[0] eq '-minor' )
-			{
-				$IncrVersionType = 0; # major number
-			}
-			else
-			{
-				die "Wrong parameter\n";
-			}
-		}
+		$IncrVersionType = 2; # major number
+		$PosArgv++;
+		next;
 	}
+	if ( $ARGV[$PosArgv] eq '-minor' )
+	{
+		$IncrVersionType = 0; # major number
+		$PosArgv++;
+		next;
+	}
+	if ( $ARGV[$PosArgv] eq '-notest' )
+	{
+		$DoTest = 0; # major number
+		print "do not test\n";
+		$PosArgv++;
+		next;
+	}
+	die "Wrong parameter\n";
 }
 
 @UsualFiles = ( 'SConstruct', 'OmiscidScons.py', 'OmiscidInit.py', 'LICENCE', 'README', 'CHANGES', 'Doxyfile' );
@@ -66,7 +74,7 @@ if ( $WorkingRep eq '' )
 	die "Unable to find working rep\n";
 }
 
-$WorkingRep = 'OMiSCID-Dev'; # 'OMiSCID';
+# $WorkingRep = 'OMiSCID-Dev'; # 'OMiSCID';
 
 if ( -e '../LastVersion.info' )
 {
@@ -141,97 +149,105 @@ foreach $file ( keys %FilesToAdd )
 
 chdir('..');
 system( $command );
-chdir("$WorkingRep");
+chdir($WorkingRep);
 
-$Computers{'astree'} = '000e0c5e4586';
-$Options{'astree'}   = ' zeroconf=avahi ';
-$Computers{'metis'}  = '000d936fc38c';
-$Options{'metis'}   = '';
-# $Computers{'desdemona'}  = '000bcd624fa9';
-# $Options{'desdemona'}   = ' zeroconf=mdns ';
-
-$TestsList{'RegisterSearchTest.cpp RegisterThread.cpp'} = 'RegisterTest';
-$TestsList{'BrowsingTest.cpp RegisterThread.cpp'} = 'BrowsingTest';
-
-$PutInEnv = "setenv LD_LIBRARY_PATH /tmp/OmiscidInstall/lib/:\$LD_LIBRARY_PATH;setenv DYLD_LIBRARY_PATH /tmp/OmiscidInstall/lib/:\$LD_LIBRARY_PATH;setenv PATH /tmp/OmiscidInstall/bin:\${PATH}:/tmp/OmiscidInstall/bin/";
-
-$NumComputer = 0;
-
-local (*READ, *WRITE, *ERROR);
-
-foreach $TestComputer ( keys %Computers )
+if ( $DoTest == 1 )
 {
-	# Check if computer if available
-	print STDERR "Trying to connect to $TestComputer.\n";
-	print STDERR "Send an etherwake command to $TestComputer.\n";
-	$MacAddress = $Computers{$TestComputer};
-	$MacAddress =~ s/://g;
-	`wol $MacAddress`;
-	$NbTry = 10;
-	while( $NbTry > 0 )
+	$Computers{'astree'} = '000e0c5e4586';
+	$Options{'astree'}   = ' zeroconf=avahi ';
+	$Computers{'metis'}  = '000d936fc38c';
+	$Options{'metis'}   = '';
+	$Computers{'desdemona'}  = '000bcd624fa9';
+	$Options{'desdemona'}   = ' zeroconf=mdns ';
+	
+	$TestsList{'RegisterSearchTest.cpp RegisterThread.cpp'} = 'RegisterTest';
+	$TestsList{'BrowsingTest.cpp RegisterThread.cpp'} = 'BrowsingTest';
+	
+	$PutInEnv = "setenv LD_LIBRARY_PATH /tmp/OmiscidInstall/lib/:\$LD_LIBRARY_PATH;setenv DYLD_LIBRARY_PATH /tmp/OmiscidInstall/lib/:\$LD_LIBRARY_PATH;setenv PATH /tmp/OmiscidInstall/bin:\${PATH}:/tmp/OmiscidInstall/bin/";
+	
+	$NumComputer = 0;
+	
+	local (*READ, *WRITE, *ERROR);
+	
+	foreach $TestComputer ( keys %Computers )
 	{
-		$res = `ssh $TestComputer "echo 'ssh is not ok.'"`;
-		if ( $res =~ /^ssh is not ok\./ )
+		# Check if computer if available
+		print STDERR "Trying to connect to $TestComputer.\n";
+		print STDERR "Send an etherwake command to $TestComputer.\n";
+		$MacAddress = $Computers{$TestComputer};
+		$MacAddress =~ s/://g;
+		`./wol $MacAddress`;
+		$NbTry = 10;
+		while( $NbTry > 0 )
+		{
+			$res = `ssh $TestComputer "echo 'ssh is not ok.'"`;
+			if ( $res =~ /^ssh is not ok\./ )
+			{
+				last;
+			}
+			print STDERR "Send an etherwake command to $TestComputer.\n";
+			`./wol $MacAddress`;
+			sleep( 10 );
+			$NbTry--;
+		}
+		if ( $NbTry == 0 )
+		{
+			$Tested = 0;
+			last;
+		}
+	        
+		# copy Archive to the test computer
+		`scp ../$VersionFile $TestComputer:/tmp/`;
+		
+		# set that it is not tested successfully
+		$Tested = 0;
+		
+		print STDERR "Try to install Omiscid.\n";
+		open($ExecCommand, "ssh $TestComputer \"cd /tmp;rm -rf $WorkingRep OmiscidInstall;unzip $VersionFile;mkdir OmiscidInstall;cd $WorkingRep;scons debug=1 trace=1 $Options{$TestComputer} install prefix=/tmp/OmiscidInstall\" |");
+		while( $ligne = <$ExecCommand> )
+		{
+			print $ligne;
+			if ( $ligne =~ /^scons: done building targets./ )
+			{
+				# test is ok
+				$Tested = 1;
+			}
+		}
+		close( $ExecCommand );
+		
+		if ( $Tested == 0 )
 		{
 			last;
 		}
-		print STDERR "Send an etherwake command to $TestComputer.\n";
-		sleep( 10 );
-		$NbTry--;
-	}
-	if ( $NbTry == 0 )
-	{
+		
+		# set that it is not tested successfully
 		$Tested = 0;
-		last;
-	}
-        
-	# copy Archive to the test computer
-	`scp ../$VersionFile $TestComputer:/tmp/`;
-	
-	# set that it is not tested successfully
-	$Tested = 0;
-	
-	print STDERR "Try to install Omiscid.\n";
-	open($ExecCommand, "ssh $TestComputer \"cd /tmp;rm -rf $WorkingRep OmiscidInstall;unzip $VersionFile;mkdir OmiscidInstall;cd $WorkingRep;scons debug=1 trace=1 $Options{$TestComputer} install prefix=/tmp/OmiscidInstall\" |");
-	while( $ligne = <$ExecCommand> )
-	{
-		print $ligne;
-		if ( $ligne =~ /^scons: done building targets./ )
+		
+		print STDERR "Try to compile and run test(s).\n";
+		foreach $test ( keys %TestsList )
 		{
-			# test is ok
-			$Tested = 1;
+			# try to compile Test Examples
+			`ssh $TestComputer "cd /tmp/$WorkingRep/Examples;rm -f ./Test;setenv LD_LIBRARY_PATH /tmp/OmiscidInstall/lib/:\$LD_LIBRARY_PATH;setenv PATH /tmp/OmiscidInstall/bin:\${PATH}:/tmp/OmiscidInstall/bin/;g++ -o Test $test \`OmiscidControl-config --all\``;
+			$res = `ssh $TestComputer "${PutInEnv};cd /tmp/OmiscidInstall/lib/;/tmp/$WorkingRep/Examples/Test"`;
+			if ( $res =~ /Test ok\./m )
+			{
+				# test is ok
+				$Tested = 1;
+			}
+		}	
+		
+		# cleaning up
+		`ssh $TestComputer "cd /tmp/$WorkingRep/;scons debug=1 trace=1 $Options{$TestComputer} install prefix=/tmp/OmiscidInstall -c;cd /tmp;rm -rf $WorkingRep OmiscidInstall $VersionFile"`;
+		
+		if ( $Tested == 0 )
+		{
+			last;
 		}
 	}
-	close( $ExecCommand );
-	
-	if ( $Tested == 0 )
-	{
-		last;
-	}
-	
-	# set that it is not tested successfully
-	$Tested = 0;
-	
-	print STDERR "Try to compile and run test(s).\n";
-	foreach $test ( keys %TestsList )
-	{
-		# try to compile Test Examples
-		`ssh $TestComputer "cd /tmp/$WorkingRep/Examples;rm -f ./Test;setenv LD_LIBRARY_PATH /tmp/OmiscidInstall/lib/:\$LD_LIBRARY_PATH;setenv PATH /tmp/OmiscidInstall/bin:\${PATH}:/tmp/OmiscidInstall/bin/;g++ -o Test $test \`OmiscidControl-config --all\``;
-		$res = `ssh $TestComputer "${PutInEnv};cd /tmp/OmiscidInstall/lib/;/tmp/$WorkingRep/Examples/Test"`;
-		if ( $res =~ /Test ok\./m )
-		{
-			# test is ok
-			$Tested = 1;
-		}
-	}	
-	
-	# cleaning up
-	`ssh $TestComputer "cd /tmp/$WorkingRep/;scons debug=1 trace=1 $Options{$TestComputer} install prefix=/tmp/OmiscidInstall -c;cd /tmp;rm -rf $WorkingRep OmiscidInstall $VersionFile"`;
-	
-	if ( $Tested == 0 )
-	{
-		last;
-	}
+}
+else
+{
+	$Tested = 1;
 }
 
 if ( $Tested == 1 )
