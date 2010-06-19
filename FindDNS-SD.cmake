5ee64a175f5da2ee970a0c510bd4e781dce9db0c
volat <volat@c4bd1236-c4f7-0310-a2fe-a60683d9aa4a>
2010-06-19 09:40:32 +0000

    Zeroconf detection improvements brought back from cmake-prima-modules
    git-svn-id: svn+ssh://oberon/svn/OMiSCID-Dev@8875 c4bd1236-c4f7-0310-a2fe-a60683d9aa4a

--- FindDNS-SD.cmake
+++ FindDNS-SD.cmake
@@ -1,8 +1,48 @@
+# - Try to find Apple's mDNSResponder library
+# Once done this will define
+#
+#  DNS-SD_FOUND - System has DNS-SD
+#  DNS-SD_INCLUDE_DIR - The DNS-SD include directory
+#  DNS-SD_LIBRARIES - The libraries needed to use DNS-SD
+#
+# You can set the DNS-SD_ROOT variable if the package is not installed under
+# a standard prefix.
+
+# Copyright (c) 2010 Matthieu Volat. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+#
+# The views and conclusions contained in the software and documentation are
+# those of the authors and should not be interpreted as representing
+# official policies, either expressed or implied, of Matthieu Volat.
+
 IF(DNS-SD_LIBRARIES)
    SET(DNS-SD_FIND_QUIETLY TRUE)
 ENDIF(DNS-SD_LIBRARIES)
 
-FIND_PATH(DNS-SD_INCLUDE_DIR dns_sd.h PATHS)
+# For now, do not consider Avahi-Compat as a valid DNS-SD library
+#FIND_PATH(DNS-SD_INCLUDE_DIR dns_sd.h PATHS 
+#   PATH_SUFFIXES avahi-compat-libdns_sd)
+FIND_PATH(DNS-SD_INCLUDE_DIR dns_sd.h PATHS ${DNS-SD_ROOT})
 
 FIND_LIBRARY(DNS-SD_LIBRARIES NAMES dns_sd)
 
